01eb117f00cef947bf2df1a1e4877630e85346be
borislavbebin <facelessbobby@gmail.com>
2025-04-09 09:00:15 +0300

    Add player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -10,13 +11,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
-        
+
         dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
