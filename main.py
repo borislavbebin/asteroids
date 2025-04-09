b23dd178c767b8e5f44066f2de69d20469174d55
borislavbebin <facelessbobby@gmail.com>
2025-04-09 09:09:00 +0300

    Add player rotation

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player1.update(dt)
         screen.fill("black")
         player1.draw(screen)
         pygame.display.flip()
 
+        # fps = 60
         dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
